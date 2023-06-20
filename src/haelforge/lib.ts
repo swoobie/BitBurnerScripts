d0bf5b90aa05eae304ba73f9ed58984001ceb74d
swoobie <swoobie@somethingspecific.com>
2023-06-19 23:33:51 -0600

    Add colors to lib for logging

--- src/haelforge/lib.ts
+++ src/haelforge/lib.ts
@@ -104,3 +104,23 @@ export function getPurchasedBasicServers(ns: NS, servers: BasicServer[]): BasicS
 export function getPurchasedPwndServers(ns: NS, servers: PwndServer[]): PwndServer[] {
     return servers.filter(s => ns.getPurchasedServers().findIndex(val => val == s.hostname) != -1);
 }
+
+export const Colors = {
+    black: "\u001b[30m",
+    red: "\u001b[31m",
+    green: "\u001b[32m",
+    yellow: "\u001b[33m",
+    blue: "\u001b[34m",
+    magenta: "\u001b[35m",
+    cyan: "\u001b[36m",
+    white: "\u001b[37m",
+    brightBlack: "\u001b[30;1m",
+    brightRed: "\u001b[31;1m",
+    brightGreen: "\u001b[32;1m",
+    brightYellow: "\u001b[33;1m",
+    brightBlue: "\u001b[34;1m",
+    brightMagenta: "\u001b[35;1m",
+    brightCyan: "\u001b[36;1m",
+    brightWhite: "\u001b[37;1m",
+    reset: "\u001b[0m"
+};
